0f7759a7e2ea03ec4158c21f8aa1acae5406c0f0
t73liu <t73liu@gmail.com>
2022-07-29 22:35:51 -0400

    feat: Use svelte instead of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://t73liu.github.io/states-ranked",
+  integrations: [svelte()]
 });
