8b539409771fdef6ac87d44de92f3631713308ea
t73liu <t73liu@gmail.com>
2022-07-29 23:32:50 -0400

    feat: Add states table

---
+++ src/components/Layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let title = "States Ranked";
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="icon" type="image/png" href="/states-ranked/favicon-32x32.png" />
+  <title>{title}</title>
+</head>
+<body>
+  <main>
+    <slot />
+  </main>
+</body>
+</html>
---
+++ src/types/state.ts
@@ -0,0 +1,7 @@
+export interface State {
+  id: string;
+  name: string;
+  population: number;
+  realEstateTax: number;
+  salesTax: number;
+}
