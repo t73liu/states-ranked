cf6fb1ce4ac7b3efd5fa3936bb5c16bb469abc2b
t73liu <t73liu@gmail.com>
2022-07-30 22:24:23 -0400

    feat: Add income and corporate taxes

--- src/types/state.ts
+++ src/types/state.ts
@@ -4,4 +4,18 @@ export interface State {
   population: number;
   realEstateTax: number;
   salesTax: number;
+  personalIncomeTax: TaxBracket[];
+  // Some states have an alternate tax brackets for higher personal income.
+  highIncomeTax?: AlternateTaxBracket;
+  corporateTax: TaxBracket[];
+}
+
+export interface AlternateTaxBracket {
+  minimumIncome: number;
+  personalIncomeTax: TaxBracket[];
+}
+
+export interface TaxBracket {
+  taxRate: number;
+  minimumIncome: number;
 }
